8a200ba93ffc6789e97002d4ce91dc7b645da33b
Saurabh Khanduja <khandujasaurabh@gmail.com>
2025-08-27 19:43:57 +0200

    Remove base model and add other models to manifest

--- __init__.py
+++ __init__.py
@@ -2,9 +2,61 @@
 Roboflow RF-DETR models.
 """
 
-import eta.core.web as etaw
+import logging
 
-from utils.rf_detr_model import RfDetrModel
+import eta.core.web as etaw
+import fiftyone as fo
+import supervision
+from fiftyone.core.models import Model
+from rfdetr import RFDETRLarge, RFDETRMedium, RFDETRNano, RFDETRSmall
+
+
+class RfDetrModel(Model):
+    """A fifty-one wrapper class for the Roboflow RF-DETR model."""
+
+    def __init__(self, model_name, model_path, **kwargs):
+        super().__init__()
+
+        def get_model(model_name):
+            model_class = {
+                "saurabheights/rf-detr-nano": RFDETRNano,
+                "saurabheights/rf-detr-small": RFDETRSmall,
+                "saurabheights/rf-detr-medium": RFDETRMedium,
+                "saurabheights/rf-detr-large": RFDETRLarge,
+            }
+
+            # TODO - Download to model_path
+            rfdetr_model = model_class[model_name]()
+            logging.info(f"Optimizing model: {model_name} for inference.")
+            rfdetr_model.optimize_for_inference()
+            logging.info(f"Model: {model_name} optimized for inference.")
+            return rfdetr_model
+
+        self._model = get_model(model_name)
+
+    @property
+    def media_type(self):
+        return "image"
+
+    def predict(self, arg):
+        h, w, c = arg.shape
+        detections: supervision.Detections = self._model.predict(arg, threshold=0.5)
+
+        # Convert detections to FiftyOne format
+        fo_detections: fo.Detections = []
+        for bbox_xyxy, class_id, confidence in zip(
+            detections.xyxy, detections.class_id, detections.confidence
+        ):
+            x1, y1, x2, y2 = bbox_xyxy
+            normalized_box = [x1 / w, y1 / h, (x2 - x1) / w, (y2 - y1) / h]
+            fo_detections.append(
+                fo.Detection(
+                    label=self._model.class_names[class_id.item()],
+                    bounding_box=normalized_box,
+                    confidence=confidence.item()
+                )
+            )
+        return fo.Detections(detections=fo_detections)
 
 
 def download_model(model_name, model_path):
@@ -31,7 +83,7 @@ def load_model(model_name, model_path, classes=None):
     """
     model_type = MODEL_TYPES[model_name]
     if model_type == "detection":
-        return RfDetrModel(model_type)
+        return RfDetrModel(model_name, model_path)
 
     return None
 
@@ -41,8 +93,6 @@ MODEL_URLS = {
     "saurabheights/rf-detr-small": "https://storage.googleapis.com/rfdetr/small_coco/checkpoint_best_regular.pth",
     "saurabheights/rf-detr-medium": "https://storage.googleapis.com/rfdetr/medium_coco/checkpoint_best_regular.pth",
     "saurabheights/rf-detr-base": "https://storage.googleapis.com/rfdetr/rf-detr-base-coco.pth",
-    # Better for fine-tuning, not so much for inference. Probably overfitted to coco.
-    "saurabheights/rf-detr-base-2": "https://storage.googleapis.com/rfdetr/rf-detr-base-2.pth",
     "saurabheights/rf-detr-large": "https://storage.googleapis.com/rfdetr/rf-detr-large.pth",
 }
 
